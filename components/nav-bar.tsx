0563368ba60f7b7d74e84109e5fd73c135156c05
Tomás García Gobet <t@gobet.ar>
2026-03-02 23:18:00 +0100

    updates logo

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -14,11 +14,9 @@ export function Navbar() {
     <nav className="fixed left-0 top-0 z-20 mx-auto flex h-[70px] w-full items-center border-b-4 border-border bg-secondary-background px-5">
       <div className="mx-auto flex w-[1300px] text-foreground max-w-full items-center justify-between">
         <div className="flex items-center xl:gap-10 gap-10">
-          <Link
-            className="text-[22px] size-8 rounded-base flex bg-main text-main-foreground border-2 border-black items-center justify-center font-heading"
-            href="/"
-          >
+          <Link className="flex items-center gap-4" href="/">
             <DicesIcon />
+            <span className="font-bold text-lg">CriCriCri</span>
           </Link>
           <div className="items-center text-base font-base xl:gap-10 lg:flex gap-10 hidden">
             {isCharacterSheet ? (
